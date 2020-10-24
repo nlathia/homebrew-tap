449c8c42f6abaa639ec73f60394e543963275657
nlathia <neal.lathia@gmail.com>
2020-10-24 23:57:12 +0100

    Set version to v0.0.0-alpha3

--- Formula/operator.rb
+++ Formula/operator.rb
@@ -1,8 +1,8 @@
 class Operator < Formula
   desc "🎯 operator is a CLI tool for creating and deploying cloud functions & docker containers for machine learning"
   homepage "https://github.com/operatorai"
-  url "https://github.com/operatorai/operator/archive/v0.0.0-alpha2.tar.gz"
-  sha256 "12cc9df34a56bf8066d9e17ac79f6c60d31da050d1f9d9f801e14a1b8869e8f7"
+  url "https://github.com/operatorai/operator/archive/v0.0.0-alpha3.tar.gz"
+  sha256 "e69e9b3b3698cf04487de9565f3711002991b54816067868859bf97ba3712d27"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -18,15 +18,6 @@ class Operator < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test operator`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     assert_match "v0.0.1-alpha", shell_output("#{bin}/operator version 2>&1", 2)
   end
 end
