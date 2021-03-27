be79f94c7f00ac8b5a0f76719c4a5c4d4c81a549
nlathia <neal.lathia@gmail.com>
2021-03-27 00:43:51 +0000

    Rename to kettle

--- Formula/kettle-cli.rb
+++ Formula/kettle-cli.rb
@@ -1,8 +1,8 @@
 class Operator < Formula
   desc "🎯 operator is a CLI tool for creating and deploying cloud functions & docker containers for machine learning"
   homepage "https://github.com/operatorai"
-  url "https://github.com/operatorai/operator/archive/v0.0.0-alpha6.tar.gz"
-  sha256 "a77fdd7637d114926591895558a58a7fe5cd36f0c1ca9b9da280bd629b28ba24"
+  url "https://github.com/operatorai/kettle-cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "5f62e0d17fa18d4f66881489109ebb230790491c2d46a7db2f49f0e47c14e842"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -13,11 +13,11 @@ class Operator < Formula
     bin_path.install Dir["*"]
     cd bin_path do
       system "go", "get", "./..."
-      system "go", "build", "-o", bin/"operator", "."
+      system "go", "build", "-o", bin/"kettle", "."
     end
   end
 
   test do
-    assert_match "v0.0.1-alpha", shell_output("#{bin}/operator version 2>&1", 2)
+    assert_match "v0.0.1-alpha", shell_output("#{bin}/kettle version 2>&1", 2)
   end
 end
